2be2219611c1b674f867de0018be648ceeaa80be
Gabin221 <serrurot19@gmail.com>
2025-01-18 17:37:37 +0100

    Update

--- app/src/main/java/com/example/carhistory/MainActivity.kt
+++ app/src/main/java/com/example/carhistory/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.carhistory
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ImageSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonAccount: TextView
+    private lateinit var buttonCarInfo: TextView
+    private lateinit var buttonAddPlein: TextView
+
+    private val logoData = listOf(
+        Pair(R.drawable.account_circle_outline, "buttonAccount"),
+        Pair(R.drawable.car_info, "buttonCarInfo"),
+        Pair(R.drawable.plus_thick, "buttonAddPlein")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +34,61 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        buttonAccount = findViewById(R.id.buttonAccount)
+        buttonCarInfo = findViewById(R.id.buttonCarInfo)
+        buttonAddPlein = findViewById(R.id.buttonAddPlein)
+
+        buttonAccount.setOnClickListener {
+            val inflater = this.layoutInflater
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder
+                .setView(inflater.inflate(R.layout.stats_window_modale, null))
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+
+        buttonCarInfo.setOnClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder
+                .setTitle("Fiche technique")
+                .setMessage("I am the message")
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+
+        buttonAddPlein.setOnClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder
+                .setTitle("Ajouter un plein")
+                .setMessage("I am the message")
+                .setPositiveButton("Positive") { dialog, which ->
+                    // Do something.
+                }
+                .setNegativeButton("Negative") { dialog, which ->
+                    // Do something else.
+                }
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+
+        initLogo()
+    }
+
+    fun initLogo() {
+        logoData.forEach { (drawableId, textViewId) ->
+            val textView = findViewById<TextView>(resources.getIdentifier(textViewId, "id", packageName))
+            val text = ""
+            val spannableString = SpannableString(" $text ")
+            val drawable = ContextCompat.getDrawable(this, drawableId)
+            drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+            val imageSpan = ImageSpan(drawable!!, ImageSpan.ALIGN_BOTTOM)
+            spannableString.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            textView.text = spannableString
         }
     }
 }
